687e0cc792375da434acae5b334104862c48b8d6
VyshnaviKamineni <vyshnavidevipriya@gmail.com>
2024-05-14 09:34:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('Kamineni T S L V Devi Priya');
+  const[age, setAge] = useState('19');
+  const[studentID, setStudentID] = useState('XYZ461');
+  
+  const handleClickEvent = ()=> {
+
+    setName('Vyshnavi Kamineni');
+    setAge('23');
+    setStudentID('ABC463');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='App'>
+      <h1> User Information</h1>
+      <h2> Default Values are</h2><br></br>
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+      <p>Student ID: {studentID}</p>
+      <button onClick={handleClickEvent}>Click Here </button>
     </div>
+    </>
   );
 }
 
