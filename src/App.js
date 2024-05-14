47aa1e7f5c997c5ebd91f6a820d453f69ca5a32e
VyshnaviKamineni <vyshnavidevipriya@gmail.com>
2024-05-14 10:04:23 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ function App() {
   const [name, setName] = useState('Kamineni T S L V Devi Priya');
   const[age, setAge] = useState('19');
   const[studentID, setStudentID] = useState('XYZ461');
+  const [updated, setUpdated] = useState(false);
   
   const handleClickEvent = ()=> {
 
     setName('Vyshnavi Kamineni');
     setAge('23');
     setStudentID('ABC463');
+    setUpdated(true);
   };
 
   return (
     <>
     <div className='App'>
       <h1> User Information</h1>
-      <h2> Default Values are</h2><br></br>
+      <h2>{updated ? 'Updated Values are' : 'Default Values are'}</h2>
       <p>Name: {name}</p>
       <p>Age: {age}</p>
       <p>Student ID: {studentID}</p>
